aa5591d89c9db571e0d7de9f864aabf2bf2fe92b
heizahn <hrbweibezahn@gmail.com>
2024-07-17 21:24:12 -0400

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
+import { useEffect, useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { getLatestGame } from './lib/metacritic';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	const [games, setGames] = useState([]);
+
+	useEffect(() => {
+		getLatestGame().then((res) => setGames(res));
+	}, []);
+	return (
+		<View style={styles.container}>
+			<StatusBar style='auto' />
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: '#000',
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
+
+	button: {
+		padding: 10,
+	},
 });
